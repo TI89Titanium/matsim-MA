09aa1da3c51f7e2b6b1b43736257f5ea943e6906
Maximilian Wolf <maximilian@wolfs.de>
2016-09-11 13:44:53 +0200

    changes to berlin carsharing

--- MATSimCarsharingBerlin/src/CarsharingBerlinControler.java
+++ MATSimCarsharingBerlin/src/CarsharingBerlinControler.java
@@ -1,16 +1,25 @@
-import java.util.Set;
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.carsharing.config.CarsharingConfigGroup;
+import org.matsim.contrib.carsharing.control.listeners.CarsharingListener;
+import org.matsim.contrib.carsharing.qsim.CarsharingQsimFactory;
+import org.matsim.contrib.carsharing.replanning.CarsharingSubtourModeChoiceStrategy;
+import org.matsim.contrib.carsharing.replanning.RandomTripToCarsharingStrategy;
 import org.matsim.contrib.carsharing.runExample.CarsharingUtils;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.population.PlanImpl;
 
 import utilityfunction.config.UtilityFunctionUtils;
 import utilityfunction.population.PopulationMatching;
-import utilityfunction.scoring.WolfLegScoringFunctionFactory;
 
 public class CarsharingBerlinControler {
 
@@ -29,22 +38,26 @@ public class CarsharingBerlinControler {
 		if(Integer.parseInt(config.getModule("qsim").getValue("numberOfThreads")) > 1)
 			Logger.getLogger( "org.matsim.core.controler" ).warn("Carsharing contrib is not stable for parallel qsim!! If the error occures please use 1 as the number of threads.");
 		
+		
+		
 		CarsharingUtils.addConfigModules(config);
 		
 		//Prepare PopulationMatching
 		PopulationMatching populationMatching = new PopulationMatching();
 		String selectedModes[] = {"D", "E"};
 		//Match Population with Survey (specific modes car and ride)
-		Scenario scenario = populationMatching.matchPopulationSpecificModes(config, csvFile, selectedModes);
+		final Scenario scenario = populationMatching.matchPopulationSpecificModes(config, csvFile, selectedModes);
 		
-		
+		//Create Carsharing Stations
+		createCarsharingStations(scenario, 1);
+				
 		//controler for MATSim
 		Controler controler = new Controler(scenario);
 		
-		//set up new scoring function WolfLegScoring with function factory;
-		controler.setScoringFunctionFactory(new WolfLegScoringFunctionFactory(scenario));
+		//set up new scoring function WolfLegScoring and carsharingLegScoring with function factory;
+		controler.setScoringFunctionFactory(new CarsharingBerlinFunctionFactory(scenario));
 		
-	//	installCarSharing(controler);
+		installCarSharing(controler);
 		
 		//run MATSim
 		controler.run();
@@ -52,60 +65,7 @@ public class CarsharingBerlinControler {
 		//TODO: run stats module
 	}
 	
-	/*
-public static void installCarSharing(final Controler controler) {		
-		
-		final Scenario scenario = controler.getScenario();
-		CarsharingXmlReaderNew reader = new CarsharingXmlReaderNew(scenario.getNetwork());
-		
-		final CarsharingConfigGroup configGroup = (CarsharingConfigGroup)
-				scenario.getConfig().getModule( CarsharingConfigGroup.GROUP_NAME );
-
-		reader.readFile(configGroup.getvehiclelocations());
-		
-		Set<String> carsharingCompanies = reader.getCompanies().keySet();
-		
-		MembershipReader membershipReader = new MembershipReader();
-		
-		membershipReader.readFile(configGroup.getmembership());
-
-		final MembershipContainer memberships = membershipReader.getMembershipContainer();
-		
-		final CostsCalculatorContainer costsCalculatorContainer = CarsharingUtils.createCompanyCostsStructure(carsharingCompanies);
-		
-		final CarsharingListener carsharingListener = new CarsharingListener();
-		final CarsharingSupplyInterface carsharingSupplyContainer = new CarsharingSupplyContainer(controler.getScenario());
-		carsharingSupplyContainer.populateSupply();
-		final KeepingTheCarModel keepingCarModel = new KeepingTheCarModelExample();
-		final ChooseTheCompany chooseCompany = new ChooseTheCompanyExample();
-		final ChooseVehicleType chooseCehicleType = new ChooseVehicleTypeExample();
-		final RouterProvider routerProvider = new RouterProviderImpl();
-		final CurrentTotalDemand currentTotalDemand = new CurrentTotalDemand(controler.getScenario().getNetwork());
-		final CarsharingManagerInterface carsharingManager = new CarsharingManagerNew();
-		final RouteCarsharingTrip routeCarsharingTrip = new RouteCarsharingTripImpl();
-		
-		//===adding carsharing objects on supply and demand infrastructure ===
-		
-		controler.addOverridingModule(new AbstractModule() {
-
-			@Override
-			public void install() {
-				bind(KeepingTheCarModel.class)
-				.toInstance(keepingCarModel);
-				bind(ChooseTheCompany.class).toInstance(chooseCompany);
-				bind(ChooseVehicleType.class).toInstance(chooseCehicleType);
-				bind(RouterProvider.class).toInstance(routerProvider);
-				bind(CurrentTotalDemand.class).toInstance(currentTotalDemand);
-				bind(RouteCarsharingTrip.class).toInstance(routeCarsharingTrip);
-				bind(CostsCalculatorContainer.class).toInstance(costsCalculatorContainer);
-				bind(MembershipContainer.class).toInstance(memberships);
-			    bind(CarsharingSupplyInterface.class).toInstance(carsharingSupplyContainer);
-			    bind(CarsharingManagerInterface.class).toInstance(carsharingManager);
-			    bind(DemandHandler.class).asEagerSingleton();
-			}			
-		});		
-		
-		//=== carsharing specific replanning strategies ===
+	public static void installCarSharing(final Controler controler) {
 		
 		controler.addOverridingModule( new AbstractModule() {
 			@Override
@@ -115,31 +75,70 @@ public static void installCarSharing(final Controler controler) {
 			}
 		});
 		
-		//=== adding qsimfactory, controller listeners and event handlers
 		controler.addOverridingModule(new AbstractModule() {
 			@Override
 			public void install() {
-				bindMobsim().toProvider(CarsharingQsimFactoryNew.class);
-		        addControlerListenerBinding().toInstance(carsharingListener);
-		        addControlerListenerBinding().to(CarsharingManagerNew.class);		        
-				bindScoringFunctionFactory().to(CarsharingScoringFunctionFactory.class);		      
-		        addEventHandlerBinding().to(PersonArrivalDepartureHandler.class);
-		        addEventHandlerBinding().to(DemandHandler.class);
+				bindMobsim().toProvider( CarsharingQsimFactory.class );
 			}
 		});
-		//=== adding carsharing specific scoring factory ===
-		controler.addOverridingModule(new AbstractModule() {
-			
-			@Override
-			public void install() {
-				        
-				bindScoringFunctionFactory().to(CarsharingScoringFunctionFactory.class);	
-			}
-		});
-
-		//=== routing moduels for carsharing trips ===
-
-		controler.addOverridingModule(CarsharingUtils.createRoutingModule());			
+		
+		controler.addOverridingModule(CarsharingUtils.createModule());
+		
+		final CarsharingConfigGroup csConfig = (CarsharingConfigGroup) controler.getConfig().getModule(CarsharingConfigGroup.GROUP_NAME);
+		controler.addControlerListener(new CarsharingListener(controler,
+				csConfig.getStatsWriterFrequency() ) ) ;
 	}
-*/
+	
+	public static void createCarsharingStations(Scenario sc, double percentage){
+		
+		if (percentage > 1){
+			System.out.println("Please enter Carsharing-Station-Percentage <= 1. Percentage is set to 1 (100%).");
+			percentage = 1.0;
+		}
+		
+		try{
+		File file = new File("input/Stations.txt");
+		
+				
+		// if file doesnt exists, then create it
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		String[] columns = {"Location[0]","Station-No.[1]","GeoX[2]","GeoY[3]","North[4]","East[5]","VehicleCount[6]","OpenParkingSpots (used only for onewaycarsharing)[7]"};
+		for (String c : columns){
+			bw.write(c);
+			bw.write("\t");
+		}
+		bw.newLine();
+		int stationCount = 1;
+		String[] stationInformation = new String[7];
+		
+		for (Person person : sc.getPopulation().getPersons().values()){
+			stationInformation[0] = Integer.toString(stationCount);
+			stationInformation[1] = stationInformation[0];
+			Activity firstActivity = ((PlanImpl)(person.getSelectedPlan())).getFirstActivity();
+			stationInformation[2] = Double.toString(firstActivity.getCoord().getX());
+			stationInformation[3] = Double.toString(firstActivity.getCoord().getY());
+			stationInformation[4] = "-";
+			stationInformation[5] = "-";
+			stationInformation[6] = "1";
+			for (String stI : stationInformation){
+				bw.write(stI);
+				bw.write("\t");
+			}
+			bw.newLine();
+			stationCount++;
+		}
+			
+		
+		bw.close();
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }

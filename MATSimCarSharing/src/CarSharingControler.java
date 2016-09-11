19bacea44d625b0a8fc6b53d643a7c29c76dfb40
Maximilian Wolf <maximilian@wolfs.de>
2016-09-11 13:52:52 +0200

    add carsharing project

---
+++ MATSimCarSharing/src/CarSharingControler.java
@@ -0,0 +1,14 @@
+
+
+import org.matsim.contrib.carsharing.runExample.RunCarsharing;
+
+public class CarSharingControler {
+
+	public static void main(String[] args) {
+
+		String inputFilePath[] = {"input/config.xml"};
+		RunCarsharing.main(inputFilePath);
+		
+	}
+
+}
